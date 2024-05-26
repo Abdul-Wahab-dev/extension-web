8a938de7111369636b3510be38500c66e132e150
abdulwahab <abdulwahabdev0@gmail.com>
2024-05-26 16:28:44 +0500

    authenticate the extension with custom token

--- app/(if_loggedIn_redirect)/layout.tsx
+++ app/(if_loggedIn_redirect)/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useAuthentication } from "@/store/auth";
 import { useEffect } from "react";
 import { auth } from "@/app/firebase/config";
+import { getCustomToken } from "@/lib/services/authentication";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,9 +11,10 @@ export default function RootLayout({
 }>) {
   const router = useRouter();
   const setCurrentUser = useAuthentication((state) => state.setCurrentUser);
+  const params = useSearchParams();
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    auth.onAuthStateChanged(async (user) => {
       if (user && user.uid) {
         const tempObj = {
           name: user.displayName!,
@@ -21,8 +23,15 @@ export default function RootLayout({
           image: user.photoURL ?? "",
           providerId: user.providerData[0].providerId,
         };
-        router.replace("/");
-        setCurrentUser(tempObj);
+        if (params.get("via_extension")) {
+          const token = await user.getIdToken();
+          const customToken = await getCustomToken(token);
+
+          router.replace(`/?custom_token=${customToken}`);
+        } else {
+          router.replace("/");
+          setCurrentUser(tempObj);
+        }
       }
     });
   }, []);

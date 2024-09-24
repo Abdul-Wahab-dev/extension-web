947e838ac07efac9b9a28cf0be7213c807f1461c
abdulwahab <abdulwahabdev0@gmail.com>
2024-09-25 00:08:18 +0500

    contact form api

--- lib/services/product.ts
+++ lib/services/product.ts
@@ -26,3 +26,39 @@ export const getProducts = async () => {
     throw error;
   }
 };
+
+export const handleContact = async (
+  email: string,
+  subject: string,
+  detail: string
+) => {
+  try {
+    const res = await fetch("http://localhost:8000/api/v1/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        subject,
+        detail,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    if (!res.ok) {
+      const response = await res.json();
+      const statusCode = response.status || 400;
+      const message =
+        response.message && response.message.length
+          ? response.message
+          : response;
+
+      throw new AppError(message, statusCode, response.validation ?? null);
+    }
+    const parsedResult = await res.json();
+    return parsedResult;
+  } catch (error) {
+    throw error;
+  }
+};
